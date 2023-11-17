5e57f278844f3e5ba268ba6bd682be3695bb1ec4
yahmann26 <ahmadyahman26@gmail.com>
2023-11-17 10:15:24 +0700

    api product

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_fic9_ecommerce_app/data/datasources/auth_local_datasource.dart';
+import 'package:flutter_fic9_ecommerce_app/presentation/auth/login_page.dart';
+import 'package:flutter_fic9_ecommerce_app/presentation/dashboard/dashboard_page.dart';
+import 'package:flutter_fic9_ecommerce_app/presentation/home/bloc/products/products_bloc.dart';
+
+import 'presentation/auth/bloc/login/login_bloc.dart';
+import 'presentation/auth/bloc/register/register_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => RegisterBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LoginBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ProductsBloc()..add(const ProductsEvent.getAll()),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: FutureBuilder<bool>(
+          future: AuthLocalDatasource().isLogin(),
+          builder: (context, snapshot) {
+            if (snapshot.data != null && snapshot.data!) {
+              return const DashboardPage();
+            } else {
+              return const LoginPage();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
